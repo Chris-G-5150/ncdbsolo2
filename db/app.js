503ecf92033157ad35602e78209849125d892d12
Chris-G-5150 <distort_everything@proton.me>
2023-11-24 14:58:56 +0000

    done

--- db/app.js
+++ db/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-
 const {
   getTopics,
   handle404,
@@ -8,7 +7,7 @@ const {
   getInfo,
   getArticles,
   getArticleIdComments,
-  postArticleIdComments
+  postArticleIdComments,
 } = require("./controller/controller.js");
 
 const {
@@ -18,19 +17,18 @@ const {
 } = require("../db/errorHandlers/errorHandlers.js");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.get("/api/topics", getTopics);
 app.get("/api", getInfo);
-app.get("/api/articles/:article_id", getArticleById)
+app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getArticles);
-app.get("/api/:article_id/comments", getArticleIdComments)
-app.post("/api/articles/:article_id/comments", postArticleIdComments) 
+app.get("/api/:article_id/comments", getArticleIdComments);
+app.post("/api/articles/:article_id/comments", postArticleIdComments);
 
 app.use(customErrorHandler);
 app.use(handlePsqlErrors);
 app.use(handleServerErrors);
-
 
 app.all("*", handle404);
 
