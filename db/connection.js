b874c6f9730bd30cae162b26d1b0a74f9c129da0
Chris-G-5150 <distort_everything@proton.me>
2023-12-07 17:12:51 +0000

    config update

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,11 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+
 const config = {}
 
 if (ENV === 'production') {
@@ -9,9 +14,6 @@ if (ENV === 'production') {
   config.max = 2;
 }
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
